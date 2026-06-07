4ddc3fc1386754fad1591b50d62c84eef1e40e76
Gabriel Martins <gabrielmonte.aluno.institute@gmail.com>
2026-06-07 08:46:57 -0300

    Initial Commit

---
+++ DevPrep/DevPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevPrepApp.swift
+//  DevPrep
+//
+//  Created by Gabriel Monte Olivio Martins on 07/06/26.
+//
+
+import SwiftUI
+
+@main
+struct DevPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
